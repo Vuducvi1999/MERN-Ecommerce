7f892fef867bcf7281cf56d9f75288926198e712
Vuducvi1999 <vuducvi20@gmail.com>
2020-10-19 23:46:47 +0700

    adsad

---
+++ client/build/precache-manifest.fd0ad3c1b9ad1e0f86696cf0e7466b3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccd8bfa3ab428a7b6aaccdf15c862a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "947b54fd48d8815d4566",
+    "url": "/static/css/main.5ecdfab3.chunk.css"
+  },
+  {
+    "revision": "e61c49a560e4a8e7ddf9",
+    "url": "/static/js/2.f3e28861.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.f3e28861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947b54fd48d8815d4566",
+    "url": "/static/js/main.83f8261e.chunk.js"
+  },
+  {
+    "revision": "7186bdaf481ff34a8a5c",
+    "url": "/static/js/runtime-main.342bb8df.js"
+  },
+  {
+    "revision": "c6c567049af37de72c892e4ed0a1c8a4",
+    "url": "/static/media/Logo.c6c56704.png"
+  },
+  {
+    "revision": "2cf5c429934e66078f5180c4c4effa8d",
+    "url": "/static/media/banner-book.2cf5c429.jpg"
+  }
+]);
