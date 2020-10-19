8a65caf39a1967da94ab22b42be9a493471179a5
Vuducvi1999 <vuducvi20@gmail.com>
2020-10-20 00:37:43 +0700

    da commit

---
+++ client/build/precache-manifest.12defcf6b775cd4b0e457ce8c194120a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aff70760cbd3cac872a103e70cfdf4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ba12466360d68eeda96",
+    "url": "/static/css/main.75835909.chunk.css"
+  },
+  {
+    "revision": "e61c49a560e4a8e7ddf9",
+    "url": "/static/js/2.f3e28861.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.f3e28861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba12466360d68eeda96",
+    "url": "/static/js/main.4ca7a704.chunk.js"
+  },
+  {
+    "revision": "7186bdaf481ff34a8a5c",
+    "url": "/static/js/runtime-main.342bb8df.js"
+  },
+  {
+    "revision": "c6c567049af37de72c892e4ed0a1c8a4",
+    "url": "/static/media/Logo.c6c56704.png"
+  },
+  {
+    "revision": "2cf5c429934e66078f5180c4c4effa8d",
+    "url": "/static/media/banner-book.2cf5c429.jpg"
+  }
+]);
