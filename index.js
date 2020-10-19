3c734e04f04ae654b02ffb53eae945caf73e1f37
Vuducvi1999 <vuducvi20@gmail.com>
2020-10-19 22:02:10 +0700

    final deploy

--- index.js
+++ index.js
@@ -33,7 +33,14 @@ mongoose
   .then(console.log("connect db success"))
   .catch(console.log("connect db fail"));
 
-app.listen(process.env.PORT, () => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+
+app.listen(process.env.PORT || 5000, () => {
   console.log(`server start port ${process.env.PORT}`);
 });
 
